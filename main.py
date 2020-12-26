87a661ebb3996ad7765b53e9ca30c5a8e064f2af
Mubashir Hasan <mubashirhasan716@gmail.com>
2020-12-26 18:06:59 +0530

    made it awesome

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ schedule.every(10).minutes.do(start)
 schedule.every().day.at('10:30').do(start)
 
 schedule.every(1).hour.do(show)
-schedule.every().monday.do(show)
+schedule.every().monday.at('11:00').do(show)
 
 while 1==1:
     schedule.run_pending()
